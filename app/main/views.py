3fb47fecf8fe918b9a873f6e88d765495329929e
ThiraTheNerd <thiragithinji@gmail.com>
2021-06-23 00:40:46 +0300

    add the blog model

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,7 @@
 from flask import render_template,request,redirect,url_for
 from ..requests import get_quotes
 from . import main
+from flask_login import login_required
 
 
 
@@ -11,5 +12,10 @@ def index():
   quotes = get_quotes()
   title = "The Blog hompage"
   return render_template('index.html', quotes = quotes, title = title)
+
+@main.route('/blogs/new/<int:id>', methods = ['GET','POST'])
+@login_required
+def new_review(id):
+  return render_template('new_blog.html')
 
 
--- app/models.py
+++ app/models.py
@@ -2,13 +2,17 @@ from . import db
 from datetime import datetime
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash,check_password_hash
+from . import login_manager
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 class Role(db.Model):
     __tablename__ = 'roles'
 
     id = db.Column(db.Integer,primary_key = True)
     name = db.Column(db.String(255))
-    users = db.relationship('User',backref = 'role',lazy="dynamic")
 
 
     def __repr__(self):
@@ -20,18 +24,13 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(255))
     email = db.Column(db.String(255), unique = True, index = True)
-    bio = db.Column(db.String(255))
-    profile_pic_path = db.Column(db.String())
     pass_secure = db.Column(db.String(255))
-    role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
+    blogs = db.Relationship('Blog',backref = 'user',lazy = "dynamic")
     
-    def __init__(self,id,username,email,bio,profile_pic_path,pass_secure,role_id):
-        self.id =id
+    def __init__(self,username,email,pass_secure):
         self.username = username
         self.email = email
-        self.bio = profile_pic_path
         self.pass_secure = pass_secure
-        self.role_id = role_id
 
     def save_user(self):
         d.session.add(self)
@@ -52,3 +51,35 @@ class User(UserMixin, db.Model):
     def __repr__(self):
         return f'User {self.username}'
 
+class Blog(db.Model):
+    '''
+    Pitch class to define Pitch Objects
+    '''
+    __tablename__ = 'blogs'
+
+    id = db.Column(db.Integer,primary_key = True)
+    title = db.Column(db.String(50))
+    subtitle = db.Column(db.String(50))
+    content =  db.Column(db.String(1000))
+    date = db.Column(db.DateTime,default=datetime.utcnow)
+    user_id = db.Column(db.Integer,db.ForeignKey("users.id"))
+
+    def save_blog(self):
+        '''
+        Function that saves pitches
+        '''
+        db.session.add(self)
+        db.session.commit()
+    
+    @classmethod
+    def get_all_blogs(cls):
+        '''
+        Function that queries the databse and returns all the pitches
+        '''
+        return Blog.query.all()
+
+    @classmethod
+    def delete_blog(cls,id):
+        '''
+        '''
+        db.session.delete()
