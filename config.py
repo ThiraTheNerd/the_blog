c69ae6b4839819016c7a539c958f2b2b3610e46f
ThiraTheNerd <thiragithinji@gmail.com>
2021-06-23 10:42:52 +0300

    remove the api url from config

--- config.py
+++ config.py
@@ -1,7 +1,6 @@
 import os
 
 class Config:
-  QUOTES_API_BASE_URL = 'http://quotes.stormconsultancy.co.uk/random.json'
   SECRET_KEY = os.environ.get('SECRET_KEY')
   SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://thirathenerd:admin2021@localhost/blog'
   UPLOADED_PHOTOS_DEST ='app/static/photos'
